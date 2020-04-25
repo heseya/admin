5c6903b56594b458aec5934753734f7b29bc8593
Artur Mędrygał <medrygal.artur@gmail.com>
2020-04-25 13:15:01 +0200

    Media uploader

---
+++ src/components/MediaUploader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="media-uploader"
+    @drop.prevent="addFile"
+    @dragenter.prevent="changeDrag(true)"
+    @dragover.prevent
+    @dragleave.prevent="changeDrag(false)"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MediaUploader',
+  data: () => ({
+    rawFiles: []
+  }),
+  methods: {
+    addFile(e) {
+      this.rawFiles = [...e.dataTransfer.files]
+      this.$emit('drop', this.rawFiles)
+      this.changeDrag(false)
+      this.upload()
+    },
+    async upload() {
+      // TODO
+      console.log('files', this.rawFiles)
+      this.$emit('upload', [1, 2])
+    },
+    changeDrag(isDrag) {
+      this.$emit('dragChange', isDrag)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.media-uploader {
+}
+</style>

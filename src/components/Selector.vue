78492c07b89499406f0784c5e7196c6890dce5f4
Artur Mędrygał <medrygal.artur@gmail.com>
2021-02-28 22:32:53 +0100

    Selector using local state

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -2,35 +2,40 @@
   <div class="schema-selector">
     <vs-input v-model="query" :label="`Wyszukaj ${typeName}`"></vs-input>
 
-    <empty v-if="query !== '' && list.length === 0">Nic nie znaleziono</empty>
+    <div ref="content">
+      <empty v-if="query !== '' && list.length === 0">Nic nie znaleziono</empty>
 
-    <list class="schema-selector__schemas" v-if="!isLoading">
-      <list-item class="schema-selector__schema" v-for="item in list" :key="item.id" no-hover>
-        {{ item.name }}
-        <small>{{ getSubText(item) }}</small>
-        <template #action>
-          <div class="flex">
-            <vs-button success icon @click="onSelect(item)">
-              Dodaj
-            </vs-button>
-          </div>
-        </template>
-      </list-item>
-    </list>
+      <list class="schema-selector__schemas">
+        <list-item class="schema-selector__schema" v-for="item in list" :key="item.id" no-hover>
+          {{ item.name }}
+          <small>{{ getSubText(item) }}</small>
+          <template #action>
+            <div class="flex">
+              <vs-button success icon @click="onSelect(item)">
+                Dodaj
+              </vs-button>
+            </div>
+          </template>
+        </list-item>
+      </list>
+    </div>
   </div>
 </template>
 
 <script>
 // import uniqBy from 'lodash/uniqBy'
 import debounce from 'lodash/debounce'
+import queryString from 'query-string'
 import List from '@/components/List.vue'
 import Empty from '@/components/Empty.vue'
 import ListItem from '@/components/ListItem.vue'
+import { api } from '../api'
 
 export default {
   name: 'Selector',
   data: () => ({
     query: '',
+    list: [],
   }),
   props: {
     type: {
@@ -46,28 +51,33 @@ export default {
       default: () => [],
     },
   },
-  computed: {
-    list() {
-      if (this.query === '') return []
-      return this.$store.getters[`${this.type}/getData`]
-        .filter((x) => !this.existing.find((y) => x.id === y.id))
-        .slice(0, 5)
-    },
-    isLoading() {
-      return this.$store.getters[`${this.type}/getIsLoading`]
-    },
-  },
   watch: {
     query(search) {
       this.getItems(search)
     },
   },
   methods: {
-    getItems: debounce(function(search) {
-      if (search === '') return
-      this.$store.dispatch(`${this.type}/fetch`, {
-        search: search,
+    getItems: debounce(async function(search) {
+      if (search === '') {
+        this.list = []
+        return
+      }
+
+      const loading = this.$vs.loading({
+        target: this.$refs.content,
+        color: 'dark',
       })
+      try {
+        const query = queryString.stringify({
+          search: search,
+        })
+        const { data } = await api.get(`/${this.type}?${query}`)
+        this.list = data.data.filter((x) => !this.existing.find((y) => x.id === y.id)).slice(0, 5)
+        loading.close()
+      } catch (error) {
+        console.error(error)
+        loading.close()
+      }
     }, 300),
     onSelect(schema) {
       this.$emit('select', schema)

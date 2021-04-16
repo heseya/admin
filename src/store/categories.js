5d607ae92cd543e336b9846f4bb5467756cb5be7
Jędrzej Buliński <git@jedrula.pl>
2021-04-17 00:35:24 +0200

    Categories order

--- src/store/categories.js
+++ src/store/categories.js
@@ -1,8 +1,18 @@
 import { createStore } from './generator'
+import { api } from '@/api'
 
 export const categories = createStore('CATEGORIES', 'categories', {
   state: {},
   getters: {},
   mutations: {},
-  actions: {}
+  actions: {
+    async setOrder({ commit }, categories) {
+      try {
+        const { data } = await api.post('/categories/order', { categories })
+        return data
+      } catch (error) {
+        return false
+      }
+    }
+  }
 })

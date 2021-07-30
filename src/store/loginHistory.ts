ceadbb5bd5427f4a5609056afda96f6a5282b530
Artur Mędrygał <medrygal.artur@gmail.com>
2021-07-30 08:48:28 +0200

    Allow to kill sessions

--- src/store/loginHistory.ts
+++ src/store/loginHistory.ts
@@ -1,8 +1,49 @@
+import { api } from '@/api'
 import { createVuexCRUD } from './generator'
 
-export const loginHistory = createVuexCRUD()('auth/login-history', {
+import { ID } from '@/interfaces/ID'
+import { AuthSession } from '@/interfaces/AuthSession'
+
+export const loginHistory = createVuexCRUD<AuthSession>()('auth/login-history', {
   state: {},
   getters: {},
   mutations: {},
-  actions: {},
+  actions: {
+    async kill({ state, commit }, id: ID) {
+      commit('SET_LOADING', true)
+      commit('SET_ERROR', null)
+      try {
+        await api.get(`/auth/kill-session/id:${id}`)
+
+        const killedSession = state.data.find((s) => s.id === id)!
+        commit('EDIT_DATA', { key: 'id', value: id, item: { ...killedSession, revoked: true } })
+
+        commit('SET_LOADING', false)
+        return true
+      } catch (error) {
+        commit('SET_ERROR', error)
+        commit('SET_LOADING', false)
+        return false
+      }
+    },
+    async killAll({ state, commit }) {
+      commit('SET_LOADING', true)
+      commit('SET_ERROR', null)
+      try {
+        await api.get(`/auth/kill-all-sessions`)
+
+        commit(
+          'SET_DATA',
+          state.data.map((s) => ({ ...s, revoked: !s.current_session })),
+        )
+
+        commit('SET_LOADING', false)
+        return true
+      } catch (error) {
+        commit('SET_ERROR', error)
+        commit('SET_LOADING', false)
+        return false
+      }
+    },
+  },
 })

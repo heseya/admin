8a00581fb8c6c5aa937479a81a462ca7fe9b562a
Artur Mędrygał <medrygal.artur@gmail.com>
2021-08-13 13:43:13 +0200

    changed directive

--- src/plugins/permissions.ts
+++ src/plugins/permissions.ts
@@ -1,19 +1,28 @@
-import { Permission, PERMISSIONS_TREE } from '@/consts/permissions'
-import { accessor } from '@/store'
-import { hasAccess } from '@/utils/hasAccess'
 import Vue from 'vue'
+
+import { accessor } from '@/store'
+
+import { Permission, PERMISSIONS_TREE } from '@/consts/permissions'
+
+import { commentNode } from '@/utils/commentNode'
+import { hasAccess } from '@/utils/hasAccess'
 
 export const hasUserAccess = (required: Permission | Permission[]) => {
   const userPermissions = accessor.auth.permissions
   return hasAccess(required)(userPermissions)
 }
 
-Vue.directive('can', {
-  bind: function (el, binding) {
-    if (!hasUserAccess(binding.value)) {
-      el.style.display = 'none'
+Vue.directive('can', function (el, binding, vnode) {
+  const behaviour = binding.modifiers.disable ? 'disable' : 'hide'
+  const ok = hasUserAccess(binding.value)
+  if (!ok) {
+    if (behaviour === 'hide') {
+      commentNode(el, vnode)
+    } else if (behaviour === 'disable') {
+      // @ts-ignore
+      el.disabled = true
     }
-  },
+  }
 })
 
 Vue.prototype.$p = PERMISSIONS_TREE

ef6925475f4179adc9f11204eb09ab5dff59826e
Artur Mędrygał <medrygal.artur@gmail.com>
2021-12-28 14:53:58 +0100

    Added filters to Items view

---
+++ src/components/modules/items/ItemsFilter.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="orders-filter">
+    <app-input
+      v-model="local.search"
+      class="span-2"
+      type="search"
+      label="Wyszukiwanie"
+      allow-clear
+      @input="debouncedSearch"
+    />
+
+    <app-select
+      v-model="local.soldout"
+      label="Status płatności"
+      add-all
+      option-filter-prop="label"
+      @change="debouncedSearch"
+    >
+      <a-select-option :value="'1'" label="Wyprzedane"> Wyprzedane </a-select-option>
+      <a-select-option :value="'0'" label="Dostępne"> Dostępne </a-select-option>
+    </app-select>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { debounce } from 'lodash'
+
+import { ALL_FILTER_VALUE } from '@/consts/filters'
+
+export type ItemsFilersType = {
+  search: string
+  soldout: string
+  sort?: string
+}
+
+export const EMPTY_ITEMS_FILTERS: ItemsFilersType = {
+  search: '',
+  soldout: ALL_FILTER_VALUE,
+  sort: undefined,
+}
+
+export default Vue.extend({
+  props: {
+    filters: {
+      type: Object,
+      default: () => ({ ...EMPTY_ITEMS_FILTERS }),
+    } as Vue.PropOptions<ItemsFilersType>,
+  },
+  data: () => ({
+    local: {
+      ...EMPTY_ITEMS_FILTERS,
+    },
+  }),
+  watch: {
+    filters(filters: ItemsFilersType) {
+      this.local = { ...this.local, ...filters }
+    },
+  },
+  mounted() {
+    this.local = { ...this.local, ...this.filters }
+  },
+  methods: {
+    makeSearch() {
+      this.$emit('search', {
+        ...this.filters,
+        ...this.local,
+      })
+    },
+
+    debouncedSearch: debounce(function (this: any) {
+      this.$nextTick(() => {
+        this.makeSearch()
+      })
+    }, 300),
+  },
+})
+</script>

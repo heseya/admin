dd54a307b3a450d1ec13ce1e377cef208a0cfb2b
Artur Mędrygał <medrygal.artur@gmail.com>
2022-02-14 12:56:43 +0100

    Added attributes store module

---
+++ src/interfaces/Attribute.ts
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+import { UUID } from './UUID'
+
+export enum AttributeType {
+  Text = 'text',
+  Number = 'number',
+}
+
+export interface Attribute {
+  id: UUID
+  name: string
+  description: string
+  type: AttributeType
+  searchable: boolean
+  options: AttributeOption[]
+}
+
+export interface AttributeOption {
+  id: UUID
+  value_text: string
+  value: number | null
+}
+
+export type AttributeDto = Omit<Attribute, 'id'>
+
+export interface AttributeOptionDto {
+  value_text: string
+  value: number | null
+}
---
+++ src/store/attributes.ts
@@ -0,0 +1,39 @@
+import { createVuexCRUD } from './generator'
+
+import {
+  Attribute,
+  AttributeDto,
+  AttributeOption,
+  AttributeOptionDto,
+} from '@/interfaces/Attribute'
+import { UUID } from '@/interfaces/UUID'
+import { api } from '@/api'
+
+export const attributes = createVuexCRUD<Attribute, AttributeDto, AttributeDto>()('attributes', {
+  state: {},
+  getters: {},
+  mutations: {
+    ADD_OPTION(state, { attributeId, option }: { attributeId: UUID; option: AttributeOption }) {
+      const listItem = state.data.find(({ id }) => id === attributeId)
+      if (listItem) listItem.options.push(option)
+      if (state.selected.id === attributeId) state.selected.options.push(option)
+    },
+  },
+  actions: {
+    async addOption(
+      { commit },
+      { attributeId, option }: { attributeId: UUID; option: AttributeOptionDto },
+    ) {
+      try {
+        const { data } = await api.post<{ data: AttributeOption }>(
+          `/attributes/id:${attributeId}/options`,
+          option,
+        )
+        commit('ADD_OPTION', { attributeId, option: data })
+        return { success: true, option: data.data } as const
+      } catch (e) {
+        return { success: false, error: e } as const
+      }
+    },
+  },
+})

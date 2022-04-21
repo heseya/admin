02f789db17f2e5db3d885d342134298fc5b0c3d7
Artur Mędrygał <medrygal.artur@gmail.com>
2022-04-21 15:09:51 +0200

    Changed list of the banners

--- src/views/banners/index.vue
+++ src/views/banners/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="narrower-page">
-    <PaginatedList :title="$t('title')" store-key="banners">
+    <PaginatedList :title="$t('title')" store-key="banners" :table="tableConfig">
       <template #nav>
         <icon-button v-can="$p.Banners.Add" to="/settings/banners/create">
           <template #icon>
@@ -8,13 +8,6 @@
           </template>
           {{ $t('add') }}
         </icon-button>
-      </template>
-
-      <template #default="{ item: role }">
-        <list-item :key="role.id" :url="`/settings/banners/${role.id}`">
-          {{ role.name }}
-          <small>{{ role.description }}</small>
-        </list-item>
       </template>
     </PaginatedList>
   </div>
@@ -24,27 +17,56 @@
 {
   "pl": {
     "title": "Bannery",
-    "add": "Dodaj banner"
+    "add": "Dodaj banner",
+    "table": {
+      "slug": "Identyfikator",
+      "quantity": "Ilosć grafik",
+      "active": "Aktywny"
+    }
   },
   "en": {
     "title": "Banners",
-    "add": "Add banner"
+    "add": "Add banner",
+    "table": {
+      "slug": "Identifier",
+      "quantity": "Quantity of graphics",
+      "active": "Active"
+    }
   }
 }
 </i18n>
 
 <script lang="ts">
 import Vue from 'vue'
-import ListItem from '@/components/layout/ListItem.vue'
 import PaginatedList from '@/components/PaginatedList.vue'
+
+import { TableConfig } from '@/interfaces/CmsTable'
+import { Banner } from '@/interfaces/Banner'
 
 export default Vue.extend({
   metaInfo(this: any) {
     return { title: this.$t('title') as string }
   },
+
   components: {
-    ListItem,
     PaginatedList,
+  },
+
+  computed: {
+    tableConfig(): TableConfig<Banner> {
+      return {
+        headers: [
+          { key: 'name', label: this.$t('common.form.name') as string },
+          { key: 'slug', label: this.$t('table.slug') as string },
+          {
+            key: 'responsive_media',
+            label: this.$t('table.quantity') as string,
+            render: (v) => v.length,
+          },
+          { key: 'active', label: this.$t('table.active') as string },
+        ],
+      }
+    },
   },
 })
 </script>

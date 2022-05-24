4057ebc22ee9837c91efbfa319ac75933fdef07f
Artur Mędrygał <medrygal.artur@gmail.com>
2022-05-24 13:08:36 +0200

    HES-1372 fixed errors messages

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,14 +1,21 @@
 import { AxiosError } from 'axios'
 import { formatApiNotification } from './utils'
 
-export type ApiError = AxiosError<{ message?: string; error: { message: string; errors: any[] } }>
+export type ApiError = AxiosError<{
+  message?: string
+  error: { message: string; errors: Record<string, { key: string; message: string }[]> }
+}>
 
 export const formatApiError = (error: ApiError) => {
   const responseData = error.response?.data
 
   return {
     title: (responseData?.error?.message || error.message) as string,
-    text: (Object.values(responseData?.error?.errors || {})[0] as string) || '',
+    text:
+      Object.values(responseData?.error?.errors || {})
+        .flat()
+        .map((v) => v.message)
+        .join(', ') || '',
   }
 }
 

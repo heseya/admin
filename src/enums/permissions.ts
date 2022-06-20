75d364e7a2a57efc4c7f2ac2600b1bf2f2cdc794
Artur Mędrygał <medrygal.artur@gmail.com>
2022-06-20 14:35:31 +0200

    Modified media permissions

--- src/enums/permissions.ts
+++ src/enums/permissions.ts
@@ -89,6 +89,7 @@ export enum CartPermission {
 
 export enum MediaPermission {
   Show = 'media.show',
+  ShowMetadataPrivate = 'media.show_metadata_private',
   Add = 'media.add',
   Edit = 'media.edit',
   Remove = 'media.remove',

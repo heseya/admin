ed73f26b6db8b2da0953677a4fb38a0c732a99b5
Artur Mędrygał <medrygal.artur@gmail.com>
2022-07-15 13:54:05 +0200

    removed unused permissions

--- src/utils/tests/hasAccess.test.ts
+++ src/utils/tests/hasAccess.test.ts
@@ -1,57 +1,35 @@
-import { AdminPermission, SettingsPermission } from '../../enums/permissions'
+import { PERMISSIONS_TREE as P } from '@/consts/permissions'
 import { hasAccess } from '../hasAccess'
 
 // write test for hasAccess
 describe('hasAccess', () => {
   it('should return true if user has permission all required permissions', () => {
-    expect(hasAccess(SettingsPermission.Show)([SettingsPermission.Show])).toBe(true)
-    expect(
-      hasAccess([SettingsPermission.Show, AdminPermission.Login])([
-        SettingsPermission.Show,
-        AdminPermission.Login,
-      ]),
-    ).toBe(true)
-    expect(
-      hasAccess([SettingsPermission.Show])([SettingsPermission.Show, AdminPermission.Login]),
-    ).toBe(true)
+    expect(hasAccess(P.Settings.Show)([P.Settings.Show])).toBe(true)
+    expect(hasAccess([P.Settings.Show, P.Admin.Login])([P.Settings.Show, P.Admin.Login])).toBe(true)
+    expect(hasAccess([P.Settings.Show])([P.Settings.Show, P.Admin.Login])).toBe(true)
   })
 
   it('should return true if user has permission any of required permissions', () => {
-    expect(
-      hasAccess([SettingsPermission.Show, AdminPermission.Login], true)([AdminPermission.Login]),
-    ).toBe(true)
-    expect(
-      hasAccess([SettingsPermission.Show, SettingsPermission.Add], true)([SettingsPermission.Show]),
-    ).toBe(true)
+    expect(hasAccess([P.Settings.Show, P.Admin.Login], true)([P.Admin.Login])).toBe(true)
+    expect(hasAccess([P.Settings.Show, P.Settings.Add], true)([P.Settings.Show])).toBe(true)
   })
 
   it('should return false if user dont have permission', () => {
-    expect(hasAccess(SettingsPermission.Show, true)([SettingsPermission.Add])).toBe(false)
-    expect(
-      hasAccess([AdminPermission.Login, SettingsPermission.Add], true)([SettingsPermission.Show]),
-    ).toBe(false)
+    expect(hasAccess(P.Settings.Show, true)([P.Settings.Add])).toBe(false)
+    expect(hasAccess([P.Admin.Login, P.Settings.Add], true)([P.Settings.Show])).toBe(false)
 
-    expect(hasAccess(SettingsPermission.Show)([SettingsPermission.Add])).toBe(false)
+    expect(hasAccess(P.Settings.Show)([P.Settings.Add])).toBe(false)
+    expect(hasAccess([P.Admin.Login, P.Settings.Add])([P.Settings.Show])).toBe(false)
+    expect(hasAccess([P.Admin.Login, P.Settings.Show])([P.Settings.Show])).toBe(false)
     expect(
-      hasAccess([AdminPermission.Login, SettingsPermission.Add])([SettingsPermission.Show]),
+      hasAccess([P.Settings.Show, P.Settings.Add, P.Admin.Login])([P.Settings.Show, P.Admin.Login]),
     ).toBe(false)
-    expect(
-      hasAccess([AdminPermission.Login, SettingsPermission.Show])([SettingsPermission.Show]),
-    ).toBe(false)
-    expect(
-      hasAccess([SettingsPermission.Show, SettingsPermission.Add, AdminPermission.Login])([
-        SettingsPermission.Show,
-        AdminPermission.Login,
-      ]),
-    ).toBe(false)
-    expect(
-      hasAccess([SettingsPermission.Show])([SettingsPermission.Edit, AdminPermission.Login]),
-    ).toBe(false)
+    expect(hasAccess([P.Settings.Show])([P.Settings.Edit, P.Admin.Login])).toBe(false)
   })
 
   it('should return false if invalid input', () => {
     expect(hasAccess({} as any)([null as any])).toBe(false)
-    expect(hasAccess(null as any, true)([SettingsPermission.Add])).toBe(false)
+    expect(hasAccess(null as any, true)([P.Settings.Add])).toBe(false)
     expect(hasAccess([{} as any], true)([{} as any])).toBe(false)
   })
 })

c499f2327f0f214a8c1f796d5b6ca6220105301b
Jakub Wójcik <jkbwojcik11@gmail.com>
2022-07-19 14:32:22 +0200

    Update audit interface

--- src/heseya-store-core.d.ts
+++ src/heseya-store-core.d.ts
@@ -1,4 +1,14 @@
 import '@heseya/store-core'
 
 // ? Here we can ovveride types in SDK, when they changed but SDK is not yet updated
-declare module '@heseya/store-core' {}
+declare module '@heseya/store-core' {
+  export interface EntityAudits<Entity> {
+    id: UUID
+    event: 'created' | 'updated'
+    created_at: string
+    old_values: Partial<Entity>
+    new_values: Partial<Entity>
+    issuer_type: string
+    issuer: UserList | App
+  }
+}

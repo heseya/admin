7d5287b743e1064a8a12ea1be5a5bdc8a2856990
Artur Mędrygał <medrygal.artur@gmail.com>
2022-08-09 13:13:06 +0200

    updated heseya core

--- src/heseya-store-core.d.ts
+++ src/heseya-store-core.d.ts
@@ -1,15 +1,4 @@
 import '@heseya/store-core'
 
 // ? Here we can ovveride types in SDK, when they changed but SDK is not yet updated
-declare module '@heseya/store-core' {
-  interface WarehouseDeposit {
-    id: UUID
-    sku: string
-    quantity: number
-    shipping_time: null | number
-    shipping_date: null | string
-    item_id: UUID
-    order: { id: UUID; code: string } | null
-    created_at: string
-  }
-}
+declare module '@heseya/store-core' {}

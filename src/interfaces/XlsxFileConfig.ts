32a81d95fb04aaf793d66b2ad84b5ce4c1dc6343
Artur Mędrygał <medrygal.artur@gmail.com>
2022-09-13 09:37:57 +0200

    Added download xlsx button to warehouse

--- src/interfaces/XlsxFileConfig.ts
+++ src/interfaces/XlsxFileConfig.ts
@@ -3,7 +3,7 @@ import { BaseItem } from '@/store/generator'
 export interface XlsxFileHeader<Item extends BaseItem = any> {
   key: keyof Item
   label: string
-  format?: (key: any, item?: Item) => string | number
+  format?: (key: any, item: Item) => string | number
 }
 
 export interface XlsxFileConfig<Item extends BaseItem = BaseItem> {

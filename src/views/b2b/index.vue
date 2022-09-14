3c24fcad609ba6f04ee1ff627b68ba14311dcbac
Artur Mędrygał <medrygal.artur@gmail.com>
2022-09-14 14:27:20 +0200

    added b2b companies list

---
+++ src/views/b2b/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="narrower-page">
+    <PaginatedList store-key="b2bCompanies" :table="tableConfig" :filters="filters">
+      <template #title>
+        <span class="gray-text">{{ $t('titlePrefix') }}</span> <span>{{ $t('title') }}</span>
+      </template>
+
+      <template #filters>
+        <div>
+          <app-input
+            v-model="filters.search"
+            class="span-2"
+            type="search"
+            :label="$t('common.search')"
+            allow-clear
+            @input="debouncedSearch"
+          />
+        </div>
+      </template>
+
+      <template #nav>
+        <icon-button v-can="$p.Roles.Add" @click="openCreateNewCompanyModal">
+          <template #icon>
+            <i class="bx bx-plus"></i>
+          </template>
+          {{ $t('add') }}
+        </icon-button>
+      </template>
+    </PaginatedList>
+  </div>
+</template>
+
+<i18n lang="json">
+{
+  "pl": {
+    "title": "Lista firm",
+    "titlePrefix": "B2B",
+    "add": "Dodaj firmę",
+    "table": {
+      "users_count": "Liczba użytkowników"
+    }
+  },
+  "en": {
+    "title": "List of companies",
+    "titlePrefix": "B2B",
+    "add": "Add company",
+    "table": {
+      "users_count": "Number of users"
+    }
+  }
+}
+</i18n>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Role } from '@heseya/store-core'
+import { debounce } from 'lodash'
+
+import PaginatedList from '@/components/PaginatedList.vue'
+import { TableConfig } from '@/interfaces/CmsTable'
+
+export default Vue.extend({
+  metaInfo(this: any) {
+    return { title: this.$t('title') as string }
+  },
+  components: {
+    PaginatedList,
+  },
+  data: () => ({
+    filters: {
+      search: '',
+    },
+  }),
+
+  computed: {
+    tableConfig(): TableConfig<Role> {
+      return {
+        rowUrlBuilder: (row) => `/b2b/companies/${row.id}`,
+        headers: [
+          { key: 'name', label: this.$t('common.form.name') as string },
+          {
+            key: 'description',
+            label: this.$t('common.form.description') as string,
+            render: (v) => v || '-',
+          },
+          { key: 'users_count', label: this.$t('table.users_count') as string },
+        ],
+      }
+    },
+  },
+
+  created() {
+    this.filters.search = (this.$route.query.search as string) || ''
+  },
+
+  methods: {
+    makeSearch() {
+      if (this.filters.search !== this.$route.query.search) {
+        this.$router.push({
+          path: this.$route.path,
+          query: { page: undefined, search: this.filters.search || undefined },
+        })
+      }
+    },
+    debouncedSearch: debounce(function (this: any) {
+      this.$nextTick(() => {
+        this.makeSearch()
+      })
+    }, 300),
+    clearFilters() {
+      this.filters.search = ''
+      this.makeSearch()
+    },
+
+    openCreateNewCompanyModal() {
+      alert('TODO')
+    },
+  },
+})
+</script>
---
+++ src/views/b2b/view.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="narrower-page"></div>
+</template>
+
+<i18n lang="json">
+{
+  "pl": {},
+  "en": {}
+}
+</i18n>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>

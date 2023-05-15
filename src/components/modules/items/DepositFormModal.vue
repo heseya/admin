7fa67207ba9ff5b53a73d26ed8c55762d9b9bcdb
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-05-15 15:00:44 +0200

    feat: changed deposits behaviour

--- src/components/modules/items/DepositFormModal.vue
+++ src/components/modules/items/DepositFormModal.vue
@@ -13,6 +13,15 @@
           :label="$t('form.quantity')"
         />
 
+        <boolean-select v-model="form.from_unlimited" :add-all="false">
+          <template #label>
+            {{ $t('form.from_unlimited') }}
+            <info-tooltip>
+              {{ $t('form.from_unlimited_tooltip') }}
+            </info-tooltip>
+          </template>
+        </boolean-select>
+
         <app-select v-model="stockTimeType" :label="$t('stockTimeType.label')">
           <a-select-option
             :value="StockTimeType.Time"
@@ -48,7 +57,7 @@
           v-model="form.shipping_time"
           :disabled="disabled"
           type="number"
-          rules="not-negative"
+          rules="required|not-negative"
         >
           <template #label>
             {{ $t('form.shipping_time') }}
@@ -81,7 +90,9 @@
       "shipping_date": "Data wysyłki",
       "shipping_date_tooltip": "Oznacza, że wysyłka będzie realizowana po danej dacie. Przedmioty są traktowane jako dostępne do tej daty włącznie, chyba że wyczerpie się ilość.",
       "shipping_time": "Czas wysyłki w dniach",
-      "shipping_time_tooltip": "Oznacza czas wysyłki w dniach przedmiotów dla podanej ilości, jeśli ilość się skończy, przedmiot staje się niedostępny w ramach tej ilości dni wysyłki."
+      "shipping_time_tooltip": "Oznacza czas wysyłki w dniach przedmiotów dla podanej ilości, jeśli ilość się skończy, przedmiot staje się niedostępny w ramach tej ilości dni wysyłki.",
+      "from_unlimited": "Z nieograniczonego stanu",
+      "from_unlimited_tooltip": "Ta zmiana dotyczy nieskończonych ilości i nie wpływa na obecną ilość produktów na magazynie"
     },
     "addedSuccessfully": "Dodano depozyt do magazynu",
     "stockTimeType": {
@@ -97,7 +108,9 @@
       "shipping_date": "Shipping date",
       "shipping_date_tooltip": "Indicates that the delivery will be realized after the given date. Items are treated as available until that date, unless the quantity is exhausted.",
       "shipping_time": "Shipping time in days",
-      "shipping_time_tooltip": "Indicates the time of delivery of items for the given quantity, if the quantity is exhausted, the item becomes unavailable for the given number of days of delivery."
+      "shipping_time_tooltip": "Indicates the time of delivery of items for the given quantity, if the quantity is exhausted, the item becomes unavailable for the given number of days of delivery.",
+      "from_unlimited": "From unlimited stock",
+      "from_unlimited_tooltip": "This change concerns unlimited quantities and does not affect the current quantity of products in stock"
     },
     "addedSuccessfully": "Added deposit to the warehouse",
     "stockTimeType": {
@@ -116,17 +129,21 @@ import { isNumber, isString } from 'lodash'
 
 import Loading from '@/components/layout/Loading.vue'
 import { formatDateTimeInput } from '@/utils/dates'
+import { WarehouseDepositDto } from '@heseya/store-core'
+import BooleanSelect from '@/components/form/BooleanSelect.vue'
 
 type InnerWarehouseDepositDto = {
   quantity: number
   shipping_time?: number | undefined
   shipping_date?: string | undefined
+  from_unlimited: '0' | '1'
 }
 
 const EMPTY_DEPOSIT_FORM: InnerWarehouseDepositDto = {
   quantity: 0,
-  shipping_time: undefined,
+  shipping_time: 0,
   shipping_date: undefined,
+  from_unlimited: '0',
 }
 
 enum StockTimeType {
@@ -135,9 +152,10 @@ enum StockTimeType {
 }
 
 export default Vue.extend({
-  components: { ValidationObserver, Loading },
+  components: { ValidationObserver, Loading, BooleanSelect },
   props: {
     defaultTime: { type: [Number, String], default: null },
+    defaultFromUnlimited: { type: Boolean, default: false },
     visible: { type: Boolean, default: false },
     disabled: { type: Boolean, default: false },
     itemId: { type: String, required: true },
@@ -160,6 +178,7 @@ export default Vue.extend({
       if (visible) {
         this.form = { ...EMPTY_DEPOSIT_FORM }
         this.stockTimeType = StockTimeType.Time
+        this.form.from_unlimited = this.defaultFromUnlimited ? '1' : '0'
         if (isString(this.defaultTime)) {
           this.stockTimeType = StockTimeType.Date
           this.form.shipping_date = formatDateTimeInput(this.defaultTime) || ''
@@ -176,9 +195,15 @@ export default Vue.extend({
     async saveDeposit() {
       this.isLoading = true
 
+      const timeKey = this.stockTimeType === StockTimeType.Time ? 'shipping_time' : 'shipping_date'
+
       const deposit = await this.$accessor.items.createDeposit({
         id: this.itemId,
-        deposit: this.form,
+        deposit: {
+          quantity: this.form.quantity,
+          [timeKey]: this.form[timeKey],
+          from_unlimited: this.form.from_unlimited === '1',
+        } as WarehouseDepositDto,
       })
 
       if (deposit) {

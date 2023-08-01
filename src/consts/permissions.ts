cfe40d1739138f55718deacb5cddb3d9c97da9ab
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-08-01 13:13:12 +0200

    feat: removed lang permissions mock

--- src/consts/permissions.ts
+++ src/consts/permissions.ts
@@ -1,9 +1,7 @@
 import { Permission, PERMISSIONS_TREE as Permissions } from '@heseya/store-core'
-import { LanguagesPermission } from '@/enums/permissions'
 
 export const PERMISSIONS_TREE = {
   ...Permissions,
-  Languages: LanguagesPermission,
 }
 
 export const ALL_PERMISSIONS: Permission[] = Object.values(PERMISSIONS_TREE)
--- src/enums/permissions.ts
+++
@@ -1,6 +0,0 @@
-export enum LanguagesPermission {
-  ShowHidden = 'languages.show_hidden',
-  Add = 'languages.add',
-  Edit = 'languages.edit',
-  Remove = 'languages.remove',
-}

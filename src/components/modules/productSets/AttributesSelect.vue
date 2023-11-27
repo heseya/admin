6042f138d50bd8e4ff254cc2296e0b8c977f9899
@mariusz.baranowski <Mariusz.Baranowski@escolasoft.com>
2023-11-27 13:57:52 +0100

    Revert "feat: fetch data from select to display human-readable label"
    This reverts commit 0ecdfced6646de849aa4990d9c6214cd095c5e0f.

--- src/components/modules/productSets/AttributesSelect.vue
+++ src/components/modules/productSets/AttributesSelect.vue
@@ -33,7 +33,6 @@
 import { defineComponent, PropType } from 'vue'
 import debounce from 'lodash/debounce'
 import { Attribute } from '@heseya/store-core'
-import { isEqual, cloneDeep } from 'lodash'
 
 export default defineComponent({
   props: {
@@ -43,14 +42,9 @@ export default defineComponent({
     },
     disabled: { type: Boolean, default: false },
   },
-
   data: () => ({
     isLoading: false,
-    loadedSelect: false,
-    defaultAttributes: [] as Attribute[],
-    loadedAttributes: [] as Attribute[],
   }),
-
   computed: {
     selectedAttributes: {
       get(): string[] {
@@ -65,56 +59,22 @@ export default defineComponent({
         this.$emit('input', value)
       },
     },
-
     attributes(): Attribute[] {
-      const initAttributes = this.$accessor.attributes.data
-      if (
-        initAttributes.length &&
-        this.selectedAttributes.length &&
-        !this.loadedSelect &&
-        !this.isLoading
-      ) {
-        this.loadAttributes(cloneDeep(initAttributes))
-        this.toggleLoaded()
-      }
-
-      return this.loadedSelect
-        ? this.defaultAttributes.concat(this.loadedAttributes)
-        : this.defaultAttributes
+      return this.$accessor.attributes.data
     },
   },
-
   created() {
     this.searchAttributes()
   },
-
   methods: {
     async searchAttributes(value?: string) {
       this.isLoading = true
       await this.$accessor.attributes.fetch({ search: value, global: 0 })
       this.isLoading = false
     },
-
     onSearch: debounce(function (this: any, value: string) {
       this.searchAttributes(value)
     }, 200),
-
-    toggleLoaded() {
-      this.loadedSelect = !this.loadedSelect
-    },
-
-    async loadAttributes(oldAttributes: Attribute[]) {
-      this.isLoading = true
-      this.defaultAttributes = oldAttributes
-      await this.$accessor.attributes.fetch({ ids: this.selectedAttributes })
-      const loadedAttributes = cloneDeep(this.$accessor.attributes.data)
-
-      if (!isEqual(loadedAttributes, oldAttributes)) {
-        this.loadedAttributes = loadedAttributes
-      }
-
-      this.isLoading = false
-    },
   },
 })
 </script>

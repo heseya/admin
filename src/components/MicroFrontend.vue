2e1097cb06346984fdcd19bc484619d0e9d3f9ee
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-12-11 12:50:01 +0100

    feat: inject microserwice url to microfront

--- src/components/MicroFrontend.vue
+++ src/components/MicroFrontend.vue
@@ -4,7 +4,9 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { findAppByHost, installApp, uninstallApp } from 'bout'
+import { findAppByHost, installApp, onMounted, openCommunicationChannel, uninstallApp } from 'bout'
+import { Language } from '@heseya/store-core'
+import { getApiURL } from '@/utils/api'
 
 export default defineComponent({
   name: 'MicroFrontend',
@@ -19,6 +21,7 @@ export default defineComponent({
     },
   },
   data: () => ({
+    wasMounted: false,
     container: null as null | string | Element,
   }),
   computed: {
@@ -28,7 +31,51 @@ export default defineComponent({
     standardHost(): string {
       return this.host.endsWith('/') ? this.host : `${this.host}/`
     },
+
+    mainChannel() {
+      return openCommunicationChannel('Main')
+    },
+    tokenChannel() {
+      return openCommunicationChannel('Token')
+    },
   },
+
+  watch: {
+    '$accessor.auth.getIdentityToken'(token: string) {
+      this.tokenChannel.emit('set', token)
+    },
+
+    '$i18n.locale'(locale: string) {
+      this.mainChannel.emit('uiLanguage:set', locale)
+    },
+    '$accessor.config.apiLanguage'(lang: Language) {
+      this.mainChannel.emit('apiLanguage:set', lang)
+    },
+  },
+
+  created() {
+    onMounted(() => {
+      if (!this.wasMounted) {
+        this.mainChannel.emit('init', {
+          coreUrl: getApiURL(),
+          // TODO: this will be injected into any microfrontend, not only this one!
+          serviceUrl: this.standardHost,
+          token: this.$accessor.auth.getIdentityToken,
+          user: this.$accessor.auth.user,
+          uiLanguage: this.$i18n.locale,
+          apiLanguage: this.$accessor.config.apiLanguage,
+        })
+
+        this.wasMounted = true
+      }
+    })
+
+    this.tokenChannel.on<undefined>('refresh', async () => {
+      const { identityToken } = await this.$accessor.auth.refreshToken()
+      return identityToken
+    })
+  },
+
   async mounted() {
     const { head, body } = this.initShadowDom()
     this.container = body

eb95a6581bc09deec5626afb2c1e07f379dd81d6
Artur Mędrygał <artur.medrygal@escolasoft.com>
2024-03-01 11:47:51 +0100

    feat: improved redirect list

--- src/views/redirects/index.vue
+++ src/views/redirects/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="narrower-page">
+  <div>
     <PaginatedList
       :title="$t('title').toString()"
       store-key="redirects"
@@ -14,6 +14,32 @@
           {{ $t('add') }}
         </icon-button>
       </template>
+
+      <template #default="{ item: redirect }">
+        <CmsTableRow
+          :key="redirect.id"
+          class="redirect-row"
+          :item="redirect"
+          :headers="tableConfig.headers"
+          :to="`/settings/redirects/${redirect.id}`"
+        >
+          <template #type="{ rawValue }">
+            <code :class="['http-code', `http-code--${rawValue}`]">{{ rawValue || '-' }}</code>
+          </template>
+
+          <template #source_url="{ rawValue }">
+            <small>
+              <code>{{ rawValue || '-' }}</code>
+            </small>
+          </template>
+
+          <template #target_url="{ rawValue }">
+            <small>
+              <code>{{ rawValue || '-' }}</code>
+            </small>
+          </template>
+        </CmsTableRow>
+      </template>
     </PaginatedList>
   </div>
 </template>
@@ -24,6 +50,7 @@
     "title": "Przekierowania",
     "add": "Dodaj przekierowanie",
     "table": {
+      "type": "Kod HTTP",
       "sourceUrl": "Źródło",
       "targetUrl": "Cel",
       "enabled": "Włączone"
@@ -33,6 +60,7 @@
     "title": "Redirects",
     "add": "Add redirect",
     "table": {
+      "type": "HTTP Code",
       "sourceUrl": "Source",
       "targetUrl": "Target",
       "enabled": "Enabled"
@@ -47,6 +75,7 @@ import { Redirect } from '@heseya/store-core'
 
 import PaginatedList from '@/components/PaginatedList.vue'
 import { TableConfig } from '@/interfaces/CmsTable'
+import CmsTableRow from '@/components/cms/CmsTableRow.vue'
 
 export default defineComponent({
   metaInfo(this: any) {
@@ -54,6 +83,7 @@ export default defineComponent({
   },
   components: {
     PaginatedList,
+    CmsTableRow,
   },
   computed: {
     tableConfig(): TableConfig<Redirect> {
@@ -63,10 +93,28 @@ export default defineComponent({
           { key: 'name', label: this.$t('common.form.name') as string },
           { key: 'source_url', label: this.$t('table.sourceUrl') as string },
           { key: 'target_url', label: this.$t('table.targetUrl') as string },
-          { key: 'enabled', label: this.$t('table.enabled') as string },
+          { key: 'type', label: this.$t('table.type') as string, width: `0.5fr` },
+          { key: 'enabled', label: this.$t('table.enabled') as string, width: `0.5fr` },
         ],
       }
     },
   },
 })
 </script>
+
+<style lang="scss">
+.redirect-row .cms-table-row__col {
+  padding: 8px;
+}
+
+.http-code {
+  &--301,
+  &--307 {
+    background-color: #f6b758;
+  }
+  &--302,
+  &--308 {
+    background-color: #91e394;
+  }
+}
+</style>

5503ad87b766f6659ac7fded4ce692dfcdb5fee1
Artur Mędrygał <artur.medrygal@escolasoft.com>
2024-03-21 16:10:28 +0100

    chore: cleaned repository

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,60 +1,9 @@
 export const getApiURL = () => {
-  if (window.runtimeConfig.apiUrl) return window.runtimeConfig.apiUrl!
-
-  switch (window.location.host) {
-    // Production clients
-    case '***REMOVED***':
-      return '***REMOVED***'
-    case '***REMOVED***':
-    case '***REMOVED***':
-      return '***REMOVED***'
-    case 'admin.***REMOVED***.eu':
-      return 'https://***REMOVED***.***REMOVED***'
-    case '***REMOVED***-admin.heseya.pl':
-      return 'https://sandbox-***REMOVED***.***REMOVED***'
-    case '***REMOVED***.heseya.com':
-      return 'https://store-api.***REMOVED***.pl'
-    case '***REMOVED***':
-      return '***REMOVED***'
-    case 'admin.***REMOVED***.com':
-      return 'https://api.***REMOVED***.com'
-    case 'admin-***REMOVED***.***REMOVED***':
-      return 'https://***REMOVED***.***REMOVED***'
-    case 'admin-***REMOVED***.***REMOVED***':
-      return 'https://***REMOVED***.***REMOVED***'
-    case '***REMOVED***':
-      return '***REMOVED***'
-
-    // Stage clients
-    case '***REMOVED***-admin.stage.heseya.pl':
-      return 'https://***REMOVED***-stage.***REMOVED***'
-    case '***REMOVED***-admin.stage.heseya.pl':
-      return 'https://demo-***REMOVED***.***REMOVED***'
-    case '***REMOVED***-admin.stage.heseya.pl':
-      return '***REMOVED***'
-    case '***REMOVED***-admin.stage.heseya.pl':
-      return 'https://demo-***REMOVED***.***REMOVED***'
-
-    // Demo clients
-    case '***REMOVED***':
-      return '***REMOVED***'
-    case '***REMOVED***':
-      return '***REMOVED***'
-    case '***REMOVED***':
-      return '***REMOVED***'
-    case '***REMOVED***':
-      return '***REMOVED***'
-    case 'admin-***REMOVED***.***REMOVED***':
-      return 'https://demo-***REMOVED***.***REMOVED***'
-    case '***REMOVED***':
-      return '***REMOVED***'
-
-    // Internal domains
-    case '***REMOVED***':
-      return '***REMOVED***'
-    case '***REMOVED***':
-      return '***REMOVED***'
-    default:
-      return '***REMOVED***'
+  if (!window.runtimeConfig.apiUrl) {
+    // eslint-disable-next-line no-console
+    console.error('API URL is not defined!')
+    throw new Error('API URL is not defined')
   }
+
+  return window.runtimeConfig.apiUrl
 }

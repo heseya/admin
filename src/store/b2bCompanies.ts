8273ae3c147b3c5b3daf645f4d42545ee17b7950
Artur Mędrygał <artur.medrygal@escolasoft.com>
2024-06-04 12:20:06 +0200

    feat: reduced use of with_translations flag

--- src/store/b2bCompanies.ts
+++ src/store/b2bCompanies.ts
@@ -1,5 +1,5 @@
 import { Role, RoleCreateDto, RoleUpdateDto } from '@heseya/store-core'
-import { createVuexCRUD } from './generator'
+import { GLOBAL_QUERY_PARAMS, createVuexCRUD } from './generator'
 
 export const b2bCompanies = createVuexCRUD<Role, RoleCreateDto, RoleUpdateDto>()(
   // B2B company is a role
@@ -10,5 +10,14 @@ export const b2bCompanies = createVuexCRUD<Role, RoleCreateDto, RoleUpdateDto>()
     mutations: {},
     actions: {},
   },
-  { get: { 'metadata.b2b_company': true }, getOne: { 'metadata.b2b_company': true } },
+  {
+    get: { lang_fallback: 'any', 'metadata.b2b_company': true },
+    getOne: {
+      ...GLOBAL_QUERY_PARAMS,
+      'metadata.b2b_company': true,
+    },
+    add: GLOBAL_QUERY_PARAMS,
+    edit: GLOBAL_QUERY_PARAMS,
+    update: GLOBAL_QUERY_PARAMS,
+  },
 )
--- src/store/tags.ts
+++ src/store/tags.ts
@@ -1,9 +1,19 @@
 import { Tag, TagCreateDto, TagUpdateDto } from '@heseya/store-core'
-import { createVuexCRUD } from './generator'
+import { GLOBAL_QUERY_PARAMS, createVuexCRUD } from './generator'
 
-export const tags = createVuexCRUD<Tag, TagCreateDto, TagUpdateDto>()('tags', {
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-})
+export const tags = createVuexCRUD<Tag, TagCreateDto, TagUpdateDto>()(
+  'tags',
+  {
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+  },
+  {
+    get: GLOBAL_QUERY_PARAMS,
+    getOne: GLOBAL_QUERY_PARAMS,
+    add: GLOBAL_QUERY_PARAMS,
+    edit: GLOBAL_QUERY_PARAMS,
+    update: GLOBAL_QUERY_PARAMS,
+  },
+)

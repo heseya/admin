5ea238031814d9aa9b0fa92dcdbef3e127a99b4c
Artur Mędrygał <artur.medrygal@escolasoft.com>
2024-07-30 13:02:55 +0200

    feat: added type to consents

--- src/components/modules/consents/Form.vue
+++ src/components/modules/consents/Form.vue
@@ -8,6 +8,27 @@
           :label="$t('common.form.name').toString()"
           :disabled="disabled"
         />
+        <div class="row">
+          <app-select v-model="form.type" :label="$t('common.form.type').toString()">
+            <a-select-option
+              :value="ConsentType.Organization"
+              :label="$t(`consentType.${ConsentType.Organization}`).toString()"
+            >
+              {{ $t(`consentType.${ConsentType.Organization}`) }}
+            </a-select-option>
+            <a-select-option
+              :value="ConsentType.User"
+              :label="$t(`consentType.${ConsentType.User}`).toString()"
+            >
+              {{ $t(`consentType.${ConsentType.User}`) }}
+            </a-select-option>
+          </app-select>
+          <switch-input
+            v-model="form.required"
+            :label="$t('common.form.required').toString()"
+            horizontal
+          />
+        </div>
 
         <br />
         <PublishedLangsForm v-model="form.published" />
@@ -17,14 +38,6 @@
         </label>
         <rich-editor v-model="formDescriptionHtml" :disabled="disabled" />
         <br />
-        <div class="switches">
-          <switch-input
-            v-model="form.required"
-            :label="$t('common.form.required').toString()"
-            horizontal
-          />
-        </div>
-        <br />
         <app-button v-if="!disabled" html-type="submit">
           {{ $t('common.save') }}
         </app-button>
@@ -33,10 +46,27 @@
   </validation-observer>
 </template>
 
+<i18n lang="json">
+{
+  "pl": {
+    "consentType": {
+      "user": "Zgoda użytkownika",
+      "organization": "Zgoda organizacji"
+    }
+  },
+  "en": {
+    "consentType": {
+      "user": "User consent",
+      "organization": "Organization consent"
+    }
+  }
+}
+</i18n>
+
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { ValidationObserver } from 'vee-validate'
-import { ConsentCreateDto } from '@heseya/store-core'
+import { ConsentCreateDto, ConsentType } from '@heseya/store-core'
 
 import Card from '@/components/layout/Card.vue'
 import RichEditor from '@/components/form/RichEditor.vue'
@@ -64,6 +94,9 @@ export default defineComponent({
     },
   },
   computed: {
+    ConsentType(): typeof ConsentType {
+      return ConsentType
+    },
     form: {
       get(): ConsentCreateDto {
         return this.value
@@ -99,7 +132,9 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.switches {
+.row {
   display: flex;
+  justify-content: space-between;
+  gap: 12px;
 }
 </style>
--- src/views/consents/index.vue
+++ src/views/consents/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="narrower-page">
-    <PaginatedList :title="$t('title').toString()" store-key="consents">
+    <PaginatedList :title="$t('title').toString()" store-key="consents" :table="tableConfig">
       <template #nav>
         <icon-button v-can="$p.Consents.Add" to="/settings/consents/create">
           <template #icon>
@@ -9,13 +9,6 @@
           {{ $t('add') }}
         </icon-button>
       </template>
-
-      <template #default="{ item: consent }">
-        <list-item :key="consent.id" :url="`/settings/consents/${consent.id}`">
-          {{ consent.name }}
-          <small>{{ consent.description }}</small>
-        </list-item>
-      </template>
     </PaginatedList>
   </div>
 </template>
@@ -23,29 +16,60 @@
 <i18n lang="json">
 {
   "pl": {
-    "title": "Zgody marketingowe",
-    "add": "Dodaj zgodę"
+    "title": "Zgody przy rejestracji",
+    "add": "Dodaj zgodę",
+    "consentType": {
+      "user": "Zgoda użytkownika",
+      "organization": "Zgoda organizacji"
+    }
   },
   "en": {
-    "title": "Marketing consents",
-    "add": "Add consent"
+    "title": "Registration consents",
+    "add": "Add consent",
+    "consentType": {
+      "user": "User consent",
+      "organization": "Organization consent"
+    }
   }
 }
 </i18n>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { Consent, ConsentType } from '@heseya/store-core'
 
-import ListItem from '@/components/layout/ListItem.vue'
 import PaginatedList from '@/components/PaginatedList.vue'
+
+import { TableConfig } from '@/interfaces/CmsTable'
 
 export default defineComponent({
   metaInfo(this: any) {
     return { title: this.$t('title') as string }
   },
   components: {
-    ListItem,
     PaginatedList,
+  },
+
+  computed: {
+    tableConfig(): TableConfig<Consent> {
+      return {
+        rowUrlBuilder: (row) => `/settings/consents/${row.id}`,
+        headers: [
+          { key: 'name', label: this.$t('common.form.name') as string },
+          {
+            key: 'type',
+            label: this.$t('common.form.type') as string,
+            render: (_v, item) => {
+              if (item.type === ConsentType.User) return this.$t('consentType.user') as string
+              if (item.type === ConsentType.Organization)
+                return this.$t('consentType.organization') as string
+              return '-'
+            },
+          },
+          { key: 'required', label: this.$t('common.form.required') as string },
+        ],
+      }
+    },
   },
 })
 </script>
